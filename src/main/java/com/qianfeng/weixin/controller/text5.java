110bad5c55d1f1a8e27acf418b5fad6c05fead9a
panxy19950208 <632105204@qq.com>
2019-03-21 15:20:07 +0800

    第五次提交

---
+++ src/main/java/com/qianfeng/weixin/controller/text5.java
@@ -0,0 +1,10 @@
+package com.qianfeng.weixin.controller;
+
+public class text5 {
+    public static void main(String[] args) {
+        int i = 0;
+        if (true){
+            System.out.println("312");
+        }
+    }
+}
